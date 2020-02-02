0d1f83c69c41c404d6db21f6411d0a7091e92e3b
Sean Hellum <seanhellum45@gmail.com>
2020-02-02 02:52:04 +0000

    add clojures.rs

---
+++ clojures.rs
@@ -0,0 +1,8 @@
+fn multiplier(number: i32) -> impl Fn(i32)->i32 {
+    move | multiplied | -> i32 {  number * multiplied }
+}
+
+fn main() {
+    let times_three = multiplier(5);
+    println!("{:?}", times_three(3))
+}
